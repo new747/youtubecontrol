8b16ed7a67a305317b4844dca2906a3efa903e2f
new747 <new747@gmail.com>
2021-10-11 00:48:18 +0900

    禁止時間の1度目はURL遷移なし

--- content.js
+++ content.js
@@ -3,6 +3,7 @@ const youtubecontrol = {
 	dataUrl: null,
 	imgSrcSchedule: null,
 	imgSrcIgnore: null,
+	goSubscriptions: false,
 	getContainsTimeTo: (from, durationHours, now) => {
 		const dtFrom = new Date(from);
 		const dtTo = new Date(dtFrom.getTime() + (durationHours * 60 * 60 * 1000));
@@ -15,31 +16,58 @@ const youtubecontrol = {
 			.then(jsonData => {youtubecontrol.jsonData = jsonData}); // JSONを保持
 	},
 	deny: (msg) => {
-		document.getElementsByClassName('video-stream html5-main-video')[0].pause()
-		if (msg != youtubecontrol.deniedMsg ) {
+		youtubecontrol.pause();
+		if (msg != youtubecontrol.deniedMsg) {
 			console.log('deny called. because ' + msg);
 		}
 		youtubecontrol.deniedMsg = msg;
 	},
+	pause: () => {
+		const video = youtubecontrol.getVideo();
+		if (video != {}) {
+			video.pause();
+		}
+	},
+	isPaused: () => {
+		return youtubecontrol.getVideo().paused;
+	},
+	getVideo: () => {
+		const video = document.getElementsByClassName('video-stream html5-main-video');
+		return video.length ? video[0] : {};
+	},
+	getAvatarImg: () => {
+		const avatarImgs = Array.from(document.querySelectorAll('img#img')).filter(e => e.alt == 'アバターの画像');
+		return avatarImgs.length ? avatarImgs[0] : {};
+	},
+	toHhmm: (tm) => {
+		if (new String(tm).match(/^[0-2]?[0-9]:[0-5][0-9]$/)) {
+			return ("0" + tm).slice(-5);
+		}
+		const dt = new Date(tm);
+		return ('00' + dt.getHours()).slice(-2) + ':' + ('00' + dt.getMinutes()).slice(-2);
+	},
 	interval: () => {
 //		console.log('interval called');
-		const img = Array.from(document.querySelectorAll('img#img')).filter(e => e.alt == 'アバターの画像')[0];
+		const img = youtubecontrol.getAvatarImg();
 		if (img.src == youtubecontrol.imgSrcIgnore) {
 			return;
 		}
 		
-		if (youtubecontrol.deniedMsg && window.location.href.match('https://www.youtube.com/watch\?')) {
+		if (youtubecontrol.goSubscriptions && !youtubecontrol.isPaused()) {
+			window.location.href = 'https://www.youtube.com/feed/subscriptions';
+		}
+		if (youtubecontrol.deniedMsg && window.location.href.match('https://www.youtube.com/watch\?') && !youtubecontrol.isPaused()) {
 			console.log('denied at ' + window.location.href);
 			youtubecontrol.deny(youtubecontrol.deniedMsg);
 			alert(youtubecontrol.deniedMsg);
 
 			youtubecontrol.deniedMsg = undefined;
-			window.location.href = 'https://www.youtube.com/feed/subscriptions';
+			youtubecontrol.goSubscriptions = true;
 			return;
 		}
 
 		const now = new Date();
-		if (now.getMinutes() % 5 == 0 && now.getSeconds() < 5) {	// 5分毎に jsonData を再取得
+		if (now.getMinutes() % 5 == 0 && now.getSeconds() < 3) {	// 5分毎に jsonData を再取得
 			youtubecontrol.fetchJsonData();
 		}
 
@@ -56,6 +84,7 @@ const youtubecontrol = {
 		const allowedTimeTo = youtubecontrol.getContainsTimeTo(forceControl.allowedFrom, forceControl.durationHours, now);
 		if (allowedTimeTo) {
 			imgTooltip = allowedTimeTo + ' まで許可';
+			youtubecontrol.goSubscriptions = false;
 		}
 
 		// schedule denied?
@@ -67,15 +96,18 @@ const youtubecontrol = {
 		} catch {
 			// ignore
 		}
-		const hhmm = ('00' + now.getHours()).slice(-2) + ':' + ('00' + now.getMinutes()).slice(-2);
+		const hhmm = youtubecontrol.toHhmm(now);
 		const schedules = youtubecontrol.imgSrcSchedule == img.src ? youtubecontrol.jsonData.schedule : youtubecontrol.jsonData.schedule2;
-		const deniedSchedule = schedules.find(elem => elem.day == dayOfWeek && elem.from <= hhmm && hhmm <= elem.to);
+		const deniedSchedule = schedules.find(elem => elem.day == dayOfWeek && youtubecontrol.toHhmm(elem.from) <= hhmm && hhmm <= youtubecontrol.toHhmm(elem.to));
 		if (!deniedTimeTo && !allowedTimeTo && deniedSchedule) {
-			youtubecontrol.deny(deniedSchedule.from + ' から ' + deniedSchedule.to + ' までは禁止時間です');
+			youtubecontrol.deny(youtubecontrol.toHhmm(deniedSchedule.from) + ' から ' + youtubecontrol.toHhmm(deniedSchedule.to) + ' までは禁止時間です');
+		} else {
+			youtubecontrol.goSubscriptions = false;
 		}
 
 		imgTooltip = (!imgTooltip ? '' : '\n') + '---- 今日の禁止時間残 ----';
-		schedules.filter(elem => elem.day == dayOfWeek && elem.to >= hhmm ).forEach(elem => imgTooltip = imgTooltip + '\n' + elem.from + '～' + elem.to);
+		schedules.filter(elem => elem.day == dayOfWeek && youtubecontrol.toHhmm(elem.to) >= hhmm)
+			.forEach(elem => imgTooltip = imgTooltip + '\n' + youtubecontrol.toHhmm(elem.from) + '～' + youtubecontrol.toHhmm(elem.to));
 		img.title = imgTooltip;
 	}
 };
@@ -83,10 +115,10 @@ const youtubecontrol = {
 window.onload = function() {
 	youtubecontrol.fetchJsonData();
 
-	const intervalId = setInterval(youtubecontrol.interval, 5 * 1000)	// 5秒周期
+	const intervalId = setInterval(youtubecontrol.interval, 3 * 1000)	// 3秒周期
 
 	const intervalId2 = setInterval(function() {
-		if (document.getElementById('img').src == youtubecontrol.imgSrcIgnore) {
+		if (youtubecontrol.getAvatarImg().src == youtubecontrol.imgSrcIgnore) {
 			return;
 		}
 
